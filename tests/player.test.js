f99a008b5dcdd58c7d7a875f21569fcedbd42b47
Alex Ramirez <alejandrp98@gmail.com>
2026-08-20 18:02:44 -0700

    Completed Player module and tests for now
    Player module was completed for now. Includes constructor and playerType field. Also added test suite for it. Might modify this module later on.

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,1 +1,18 @@
 import Player from "../src/model/player.js";
+
+describe("constructor", () => {
+    test("Invalid arguments passed into constructor", () => {
+        expect(() => new Player(25)).toThrow(TypeError);
+        expect(() => new Player("something")).toThrow(Error);
+    });
+});
+
+describe("playerType", () => {
+    test("playerType is real or computer", () => {
+        const realPlayer = new Player("real");
+        expect(realPlayer.playerType).toBe("real");
+        
+        const computerPlayer = new Player("computer");
+        expect(computerPlayer.playerType).toBe("computer");
+    });
+});
